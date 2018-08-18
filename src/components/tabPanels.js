c6409387292b9cdc0231d0df567047338dc91c4e
十年 <18715096913@163.com>
2018-08-18 21:55:06 +0800

    second commit

--- src/components/tabPanels.js
+++ src/components/tabPanels.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
+import Home from "./home/Home";
+import FundingCreatorScene from "./creator/FundingCreatorScene";
+import FundingList from "./common/FundingList";
 
 const panes = [
-    { menuItem: '众筹项目', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: '我发起的', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: '我参与的', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    { menuItem: '众筹项目', render: () => <Tab.Pane><Home/></Tab.Pane> },
+    { menuItem: '我发起的', render: () => <Tab.Pane><FundingCreatorScene/></Tab.Pane> },
+    { menuItem: '我参与的', render: () => <Tab.Pane><FundingList/></Tab.Pane> },
 ]
 
 const TabPanels = () => <Tab panes={panes} />
